3c4e9bf86f493b20bf005c54428316eff38f652e
s10981132 <71520512+s10981132@users.noreply.github.com>
2021-10-29 13:19:57 -0400

    V0.2

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Keon Hill, 10/29/21 1:01pm, Version 0.1
+# Variables Practice, Keon Hill, 10/29/21 1:01pm, Version 0.2
 
 
 # Basic Variables 
@@ -6,3 +6,11 @@
 my_int = -15 # INTEGERS, any +/- whole number and 0
 my_float = 1.1234567 # FlOATING POINT, any +/- number with a decimal
 my_string = "How much would a grilled chesse be without cheese?"
+
+print(my_int) 
+print(my_float) 
+print(my_string) 
+
+my_int = -15
+my_float = 12.343534
+my_string = "life is good"

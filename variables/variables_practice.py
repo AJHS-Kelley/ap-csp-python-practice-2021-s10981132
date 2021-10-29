95b87ea6c31461ffbe16065895251624d7650c7c
s10981132 <71520512+s10981132@users.noreply.github.com>
2021-10-29 13:03:08 -0400

    V0.1
    Added my_int, my_float, and my_string to the code

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Keon Hill, 10/29/21 12:49pm, Version 0.0
+# Variables Practice, Keon Hill, 10/29/21 1:01pm, Version 0.1
+
+
+# Basic Variables 
+
+my_int = -15 # INTEGERS, any +/- whole number and 0
+my_float = 1.1234567 # FlOATING POINT, any +/- number with a decimal
+my_string = "How much would a grilled chesse be without cheese?"
